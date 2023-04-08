ea32a7cb41223d4c99cd4d64f1e4b501561ead72
susahin80 <susahin80@outlook.com.tr>
2023-04-08 14:46:39 +0300

    first commit

---
+++ eks-cluster.tf
@@ -0,0 +1,52 @@
+/* terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+} */
+
+/* provider "kubernetes" {
+  host = data.aws_eks_cluster.myapp-cluster.endpoint #api server endpoint
+  #https://registry.terraform.io/providers/hashicorp/aws/latest/docs/data-sources/eks_cluster#endpoint
+  token                  = data.aws_eks_cluster_auth.myapp-cluster.token
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.myapp-cluster.certificate_authority.0.data)
+}
+
+data "aws_eks_cluster" "myapp-cluster" {
+  # https://registry.terraform.io/modules/terraform-aws-modules/eks/aws/latest?tab=outputs
+  name = module.eks.cluster_id
+}
+
+data "aws_eks_cluster_auth" "myapp-cluster" {
+  name = module.eks.cluster_id
+} */
+
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "19.12.0"
+
+  cluster_name                   = "myapp-eks-cluster"
+  cluster_version                = "1.25"
+  cluster_endpoint_public_access = true
+
+  # https://registry.terraform.io/modules/terraform-aws-modules/vpc/aws/latest?tab=outputs
+  subnet_ids = module.myapp-vpc.private_subnets
+  vpc_id     = module.myapp-vpc.vpc_id
+
+  tags = {
+    environment = "development"
+    application = "myapp"
+  }
+
+  eks_managed_node_groups = {
+    dev = {
+      min_size     = 1
+      max_size     = 2
+      desired_size = 2
+
+      instance_types = ["t2.micro"]
+    }
+  }
+}
---
+++ vpc.tf
@@ -0,0 +1,41 @@
+provider "aws" {
+  region = "eu-central-1"
+}
+
+variable "vpc_cidr_block" {}
+variable "private_subnet_cidr_blocks" {}
+variable "public_subnet_cidr_blocks" {}
+
+data "aws_availability_zones" "azs" {}
+
+module "myapp-vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "4.0.1"
+
+
+  name            = "myapp-vpc"
+  cidr            = var.vpc_cidr_block
+  private_subnets = var.private_subnet_cidr_blocks
+  public_subnets  = var.public_subnet_cidr_blocks
+  azs             = data.aws_availability_zones.azs.names # ["eu-central-1a", "eu-central-1b", "eu-central-1c"]
+
+  enable_nat_gateway = true
+  /* enable_vpn_gateway = true */
+  single_nat_gateway   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    "kubernetes.io/cluster/myapp-eks-cluster" = "shared"
+  }
+
+  public_subnet_tags = {
+    "kubernetes.io/cluster/myapp-eks-cluster" = "shared"
+    "kubernetes.io/role/elb"                  = 1
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/cluster/myapp-eks-cluster" = "shared"
+    "kubernetes.io/role/internal-elb"         = 1
+  }
+
+}
